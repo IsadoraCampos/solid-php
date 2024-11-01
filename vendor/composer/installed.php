6d34b34916d5a913491fd385e5d97bcf4f99ec95
IsadoraCampos <sbeghenisadora@gmail.com>
2024-11-01 17:02:39 -0300

    vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cviniciussdias/alura-solid',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'aa3fe3a33cd985ccc172b2877e43deaef7cdecab',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cviniciussdias/alura-solid' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'aa3fe3a33cd985ccc172b2877e43deaef7cdecab',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
